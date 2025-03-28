27917a5856bd65b43b42e4a8c68fe5e496d72b0d
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2025-03-28 06:02:37 +0300

    Add files via upload

---
+++ BookCart/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using BookCart.Interfaces;
+using BookCart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCart.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController(IUserService userService, ICartService cartService) : Controller
+    {
+        readonly IUserService _userService = userService;
+        readonly ICartService _cartService = cartService;
+
+        /// <summary>
+        /// Get the count of item in the shopping cart
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>The count of items in shopping cart</returns>
+        [HttpGet("{userId}")]
+        public int Get(int userId)
+        {
+            int cartItemCount = _cartService.GetCartItemCount(userId);
+            return cartItemCount;
+        }
+
+        /// <summary>
+        /// Check the availability of the username
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("validateUserName/{userName}")]
+        public bool ValidateUserName(string userName)
+        {
+            return _userService.CheckUserNameAvailabity(userName);
+        }
+
+        /// <summary>
+        /// Register a new user
+        /// </summary>
+        /// <param name="registrationData"></param>
+        [HttpPost]
+        public ActionResult Post([FromBody] UserRegistration registrationData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            UserMaster user = new()
+            {
+                FirstName = registrationData.FirstName,
+                LastName = registrationData.LastName,
+                Username = registrationData.Username,
+                Password = registrationData.Password,
+                Gender = registrationData.Gender,
+                UserTypeId = 2
+            };
+
+            _userService.RegisterUser(user);
+
+            return Ok();
+        }
+    }
+}
---
+++ BookCart/DataAccess/UserDataAccessLayer.cs
@@ -0,0 +1,69 @@
+﻿using BookCart.Dto;
+using BookCart.Interfaces;
+using BookCart.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCart.DataAccess
+{
+    public class UserDataAccessLayer(BookDBContext dbContext) : IUserService
+    {
+        readonly BookDBContext _dbContext = dbContext;
+
+        public AuthenticatedUser AuthenticateUser(UserLogin loginCredentials)
+        {
+            AuthenticatedUser authenticatedUser = new();
+
+            var userDetails = _dbContext.UserMaster.FirstOrDefault(
+                u => u.Username == loginCredentials.Username && u.Password == loginCredentials.Password
+                );
+
+            if (userDetails != null)
+            {
+
+                authenticatedUser = new AuthenticatedUser
+                {
+                    Username = userDetails.Username,
+                    UserId = userDetails.UserId,
+                    UserTypeName = userDetails.UserTypeId == 1 ? "Admin" : "User"
+                };
+            }
+            return authenticatedUser;
+        }
+
+        public async Task<bool> RegisterUser(UserMaster userData)
+        {
+            bool isUserNameAvailable = CheckUserNameAvailabity(userData.Username);
+            try
+            {
+                if (isUserNameAvailable)
+                {
+                    await _dbContext.UserMaster.AddAsync(userData);
+                    await _dbContext.SaveChangesAsync();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public bool CheckUserNameAvailabity(string userName)
+        {
+            UserMaster user = _dbContext.UserMaster.FirstOrDefault(x => x.Username == userName);
+
+            return user == null;
+        }
+
+        public async Task<bool> isUserExists(int userId)
+        {
+            UserMaster user = await _dbContext.UserMaster.FirstOrDefaultAsync(x => x.UserId == userId);
+
+            return user != null;
+        }
+    }
+}
